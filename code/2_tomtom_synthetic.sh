0044d33be89d45d440e183edf48fed0d14c994f3
p-koo <koo@cshl.edu>
2019-12-24 16:17:50 -0500

    updates

--- code/2_tomtom_synthetic.sh
+++ code/2_tomtom_synthetic.sh
@@ -8,7 +8,7 @@ do
     # tomtom for most models
     for MODEL in cnn_1 cnn_2 cnn_4 cnn_10 cnn_25 cnn_50 cnn_100 cnn9_4 cnn9_25 cnn3_50 cnn3_2 cnn_50_2 cnn_2_1
     do 
-        tomtom -evalue -thresh 0.1 -o $dirpath/$MODEL $dirpath/$MODEL_clip.meme ../data/JASPAR_CORE_2016_vertebrates.meme
+        tomtom -evalue -thresh 0.1 -o $dirpath/$MODEL $dirpath/${MODEL}_clip.meme ../data/JASPAR_CORE_2016_vertebrates.meme
     done
  
     # 2nd layer filters of cnn-1
